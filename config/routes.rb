9eaf99a6c4ccfe6a336bfa123bee75d84351af82
Julie Simon <sim.ju@live.fr>
2024-11-12 15:44:10 +0100

    exo task manager ok

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,10 @@ Rails.application.routes.draw do
   # root "posts#index"
 
   get '/tasks', to: 'tasks#index'
+  get '/tasks/new', to: 'tasks#new'
   get '/tasks/:id', to: 'tasks#show', as: :task
-  get 'tasks/new', to: 'tasks#new'
-  
+  post '/tasks', to: 'tasks#create'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch '/tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
